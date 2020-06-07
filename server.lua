523dccaf9959b0fcb4306f1a53f978fd26ac7aa5
Berkeratay <64588333+Berkeratay@users.noreply.github.com>
2020-06-07 01:46:34 -0700

    Fixed 3dme and 3ddo color overlap bug
    Fixed 3dme and 3ddo color overlap bug

--- server.lua
+++ server.lua
@@ -1,3 +1,4 @@
+  
 -- ############################################
 -- --------------------------------------------
 -- 3dme : /me command but its 3D printed
@@ -26,6 +27,13 @@ end
 -- --------------------------------------------
 
 RegisterCommand('me', function(source, args)
-    local text = "* the person" .. TableToString(args) .. " *"
-    TriggerClientEvent('3dme:shareDisplay', -1, text, source)
+    local text = "* " .. TableToString(args) .. " *"
+    local cmdtype = 'me'
+     TriggerClientEvent('3dme:shareDisplay', -1, text, source, cmdtype)
+end)
+
+RegisterCommand('do', function(source, args)
+    local text = "* " .. TableToString(args) .. " *"
+    local cmdtype = 'do'
+    TriggerClientEvent('3dme:shareDisplay', -1, text, source, cmdtype)
 end)
